bf95529bd97a8efe9b329a86257b4fa1912a78aa
ItzLightyHD <itzlightyhd@apexie.eu>
2022-05-12 07:32:51 +0200

    Improved DevTools warning + New carousel

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@
 import _ from "lodash";
 
 // Warning for users who open the DevTools console of the website
-console.log("WARNING!!!\n\nThis console is meant to be used by developers to debug their code. If someone else tells you to open the DevTools and paste code here, please do not do so.");
+console.log("%cWARNING!!!" + 
+            "\n\n%cThis console is meant to be used by developers to debug their code. If someone else tells you to open the DevTools and paste code here, please do not do so.",
+            "color: red; font-size: 30px;",
+            "color: black; font-size: 15px;");
 
 // Service worker registration
 if("serviceWorker" in navigator) {
