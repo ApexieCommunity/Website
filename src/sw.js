63247ddc228bc7269432bfe8b402f415770df385
ItzLightyHD <itzlightyhd@apexie.eu>
2022-05-14 16:26:35 +0200

    Some changes...

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,18 @@ importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
+const navigationRoute = new workbox.routing.NavigationRoute(
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'apexie-navigations'
+    })
 );
+  
+const imageAssetRoute = new workbox.routing.Route(({request}) => {
+    return request.destination === 'image';
+    }, new workbox.strategies.CacheFirst({
+        cacheName: 'apexie-image-assets'
+    })
+);
+  
+workbox.routing.registerRoute(navigationRoute);
+workbox.routing.registerRoute(imageAssetRoute);
